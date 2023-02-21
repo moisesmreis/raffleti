ced7e1a54bed157045e4f83aceba414f8cf9b2fa
Moisés Reis <118673344+moisesmreis@users.noreply.github.com>
2023-02-21 19:33:59 -0300

    Add TailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,18 @@ export default {
   nitro: {
     preset: 'vercel-edge',
   },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1.0',
+      title: 'Raffleti // La Monhang ✨',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'Deixe a sorte escolher por você 🤞' }
+      ],
+    }
+  }
 };
